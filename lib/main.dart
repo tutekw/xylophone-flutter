6c13b00f9b0db5292b9c50c9fa08715127d73d66
Tomek <tuteq13@gmail.com>
2020-08-11 17:24:59 +0200

    Code refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
-  Widget _buildSoundButton(int noteNumber, Color buttonColor) {
+  Widget _buildSoundButton({int noteNumber, Color buttonColor}) {
     return Expanded(
       child: FlatButton(
         child: Text('  '),
@@ -30,13 +30,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              _buildSoundButton(1, Colors.red),
-              _buildSoundButton(2, Colors.orange),
-              _buildSoundButton(3, Colors.yellow),
-              _buildSoundButton(4, Colors.green),
-              _buildSoundButton(5, Colors.teal),
-              _buildSoundButton(6, Colors.pink),
-              _buildSoundButton(7, Colors.purple),
+              _buildSoundButton(noteNumber: 1, buttonColor: Colors.red),
+              _buildSoundButton(noteNumber: 2, buttonColor: Colors.orange),
+              _buildSoundButton(noteNumber: 3, buttonColor: Colors.yellow),
+              _buildSoundButton(noteNumber: 4, buttonColor: Colors.green),
+              _buildSoundButton(noteNumber: 5, buttonColor: Colors.teal),
+              _buildSoundButton(noteNumber: 6, buttonColor: Colors.pink),
+              _buildSoundButton(noteNumber: 7, buttonColor: Colors.purple),
             ],
           ),
         ),
